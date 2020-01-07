c930c85f6e8ab33a8e6a0ca812e966dab56ccd05
Anuj kapoor <kapoor.anuj0@gmail.com>
2020-01-07 18:59:43 +0530

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,39 @@
+/*
+Just Another Source code by -
+ __________                   __________      ___.           
+\______   \ ____ _____ ______\______   \ ____\_ |__   ______
+ |    |  _// __ \__  \_  __ \    |  _//  _ \| __ \ /  ___/
+ |    |   \  ___/ / __ \|  | \/    |   (  <_> ) \_\ \___ \ 
+ |______  /\___  >____  /__|  |______  /\____/|___  /____  >
+        \/     \/     \/             \/           \/     \/ 
+
+   _____                   __   ____  __.                                  
+  /  _  \   ____  __ __   |__| |    |/ _|____  ______   ____   ___________ 
+ /  /_\  \ /    \|  |  \  |  | |      < \__  \ \____ \ /  _ \ /  _ \_  __ \
+/    |    \   |  \  |  /  |  | |    |  \ / __ \|  |_> >  <_> |  <_> )  | \/
+\____|__  /___|  /____/\__|  | |____|__ (____  /   __/ \____/ \____/|__|   
+        \/     \/     \______|         \/    \/|__|                        
+*/
+#include<bits/stdc++.h>
+#define ll long long
+#define MAX 1000003
+#define pii pair<int,int>
+#define VP vector< pii >
+#define MOD 1000000007
+#define mp make_pair
+#define pb push_back
+#define rep(i,a,b) for(int (i) = (a); (i) < (b); (i)++)
+#define all(v) (v).begin(),(v).end()
+#define S(x) scanf("%d",&(x))
+#define S2(x,y) scanf("%d%d",&(x),&(y))
+#define SL(x) scanf("%lld",&(x))
+#define SL2(x) scanf("%lld%lld",&(x),&(y))
+#define P(x) printf("%d\n",(x))
+#define FF first
+#define SS second
+using namespace std;
+
+int main(){
+cout<<"hello";
+return 0;
+}
